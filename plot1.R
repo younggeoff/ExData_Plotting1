306f1a7c5f555a5d881511f8e0d0c5a4f9c7cd13
Geoff Marino <Geoff Marino>
2016-02-08 02:09:02 -0500

    Edit Commit

--- plot1.R
+++ plot1.R
@@ -1,7 +1,12 @@
-# Minutes  = 396+(((15+30)*(60*24)))
+# Plot 1 - Using Base Package
 
-data <- read.table("/Users/GeoffMarino/jhu datascience/household_power_consumption.txt", header = TRUE, sep = ";", skip = 66636, nrows = 2880)
+# File reads data corresponding to 01/02/2007 and 02/02/2007
+# Skip amount calculated as 396+(((15+31)*(60*24))) -- Minutes
+setwd("/Users/GeoffMarino/jhu datascience/exploratory/")
+data <- read.table("/Users/GeoffMarino/jhu datascience/exploratory/household_power_consumption.txt", header = TRUE, sep = ";", skip = 66636, nrows = 2880)
 colnames(data) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 
-# Plot 1 - Using Base Package
+
+png("plot1.png", width = 480, height = 480)
 hist(data$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red")
+dev.off()
